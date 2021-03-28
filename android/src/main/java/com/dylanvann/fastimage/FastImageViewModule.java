ec2b1d12839ba6bac8fbdad59497a2c5b03c61d6
Kevin Perreau <kperreau@users.noreply.github.com>
2021-03-28 01:11:48 +0100

    Update FastImageViewModule.java

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.model.GlideUrl;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -11,12 +12,16 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.views.imagehelper.ImageSource;
 
+import java.io.File;
+
 class FastImageViewModule extends ReactContextBaseJavaModule {
 
     private static final String REACT_CLASS = "FastImageView";
+    private static ReactApplicationContext mContext = null;
 
     FastImageViewModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        mContext = reactContext;
     }
 
     @Override
@@ -52,5 +57,53 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
                 }
             }
         });
+    }
+
+    public double getDirSize(File dir) {
+        if (dir == null) {
+            return 0;
+        }
+        if (!dir.isDirectory()) {
+            return 0;
+        }
+        double dirSize = 0;
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            if (file.isFile()) {
+                dirSize += file.length();
+            } else if (file.isDirectory()) {
+                dirSize += file.length();
+                dirSize += getDirSize(file); // 递归调用继续统计
+            }
+        }
+        return dirSize;
+    }
+
+    @ReactMethod
+    public void getCacheSize (Promise promise) {
+        try {
+            File cacheDir = Glide.getPhotoCacheDir(mContext);
+            File parentFile = cacheDir.getParentFile();
+            double size = getDirSize(parentFile);
+            promise.resolve(size);
+        } catch (Exception e) {
+            promise.reject(e);
+        }
+    }
+
+    @ReactMethod
+    public void clearCache (final Promise promise) {
+        try {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Glide.get(mContext).clearDiskCache();
+                    promise.resolve(true);
+                }
+            }).start();
+
+        } catch (Exception e) {
+            promise.reject(e);
+        }
     }
 }
